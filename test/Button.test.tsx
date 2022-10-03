1447ee3c352f8819fb586dcb972527a142fbe547
Carlos Cobo <charlie.cobo@gmail.com>
2022-10-03 16:00:54 -0500

    ADD: Testing Library

---
+++ test/Button.test.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { render, screen} from '@testing-library/react'
+import { Button, ButtonProps } from '../src'
+
+describe('<Button />', () => { 
+    const initProps: ButtonProps = {
+        label: 'Test Button',
+        role: "button"
+    }
+
+    test('Shoul be match with snapshot', () => { 
+        render(<Button {...initProps}/>);
+
+        const component = screen.getByRole('button')
+
+        expect(component).toMatchSnapshot()
+     })
+ })
