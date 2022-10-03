2251442b7dd5684e00f0517674ba62da84ef44ce
Carlos Cobo <charlie.cobo@gmail.com>
2022-10-03 16:19:17 -0500

    ADD: prettier format

--- test/Button.test.tsx
+++ test/Button.test.tsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import { render, screen} from '@testing-library/react'
-import { Button, ButtonProps } from '../src'
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { Button, ButtonProps } from '../src';
 
-describe('<Button />', () => { 
-    const initProps: ButtonProps = {
-        label: 'Test Button',
-        role: "button"
-    }
+describe('<Button />', () => {
+  const initProps: ButtonProps = {
+    label: 'Test Button',
+    role: 'button',
+  };
 
-    test('Shoul be match with snapshot', () => { 
-        render(<Button {...initProps}/>);
+  test('Shoul be match with snapshot', () => {
+    render(<Button {...initProps} />);
 
-        const component = screen.getByRole('button')
+    const component = screen.getByRole('button');
 
-        expect(component).toMatchSnapshot()
-     })
- })
+    expect(component).toMatchSnapshot();
+  });
+});
